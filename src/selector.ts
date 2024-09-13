d25584b1974085f68703d90b18e1929b5e2d3de4
Crazy Urus <crazyurus@vip.qq.com>
2024-09-13 16:17:29 +0800

    chore: 使用 corepack

--- src/selector.ts
+++ src/selector.ts
@@ -16,6 +16,7 @@ export type Selector = string | DeepSelector;
 export class SelectorPart {
   value: string;
   optimized: boolean;
+
   constructor(value: string, optimized: boolean) {
     this.value = value;
     this.optimized = optimized || false;
